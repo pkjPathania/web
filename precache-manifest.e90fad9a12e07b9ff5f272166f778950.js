7475b4d3ff5d342b6805095d29bbd28e662e23e0
Pankaj <pankaj.pathania@target-energysolutions.com>
2019-12-19 10:24:17 +0530

    Updates

---
+++ precache-manifest.e90fad9a12e07b9ff5f272166f778950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa9c4939e4d8e41765f8a602ed752bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b30158b48be00074aa",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "66b313409784630c4e0b",
+    "url": "/static/js/2.86707405.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.86707405.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11b30158b48be00074aa",
+    "url": "/static/js/main.03d93d24.chunk.js"
+  },
+  {
+    "revision": "f00c6c383f4b75a9a80c",
+    "url": "/static/js/runtime-main.4b4016b3.js"
+  }
+]);
